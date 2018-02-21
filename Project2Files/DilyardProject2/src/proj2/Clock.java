eaf17781482ee6096a5e71a04e5a4e45b7bb917a
IDZeta <36170408+IDZeta@users.noreply.github.com>
2018-02-20 22:08:56 -0500

    Clock Updates
    -Moved Scheduler code to incrementTime method
         -Allows the Clock to shuffle Processes continuously and step-by-step
         -See notes for Interface changes for more details

--- Project2Files/DilyardProject2/src/proj2/Clock.java
+++ Project2Files/DilyardProject2/src/proj2/Clock.java
@@ -1,7 +1,11 @@
 /**Class definition for the Clock Class.
  * A thread that acts like a clock for our system. All the usual getters and setters
  * are here, as well as some other methods to start and stop the clock. To constantly
- * move Processes from state to state, the clock has its own Scheduler.
+ * move Processes from state to state, the clock uses a Scheduler. This can be
+ * called continuously while the clock runs, or it can be called once by calling
+ * incrementTime().
+ * Code written by Ian Dilyard, Seth Rhodes, and Tychell Nichols for CSC 370.
+ * Project due 2/23/17. Unless specified elsewhere, all code here is original.
  * Base code taken from:
  * http://raider.mountunion.edu/csc/CSC370/Spring2018/projects/project01/ClockStarter.java
  */
@@ -13,8 +17,7 @@ import javax.swing.JLabel;
 public class Clock implements Runnable{
     
     private Thread thread;
-    private int currentTime;
-    private int clockSpeed;
+    private int currentTime, clockSpeed, timeQuantum;
     private Boolean paused;
     private JLabel label;
     private ArrayList<Process> allProcesses, newProcessList, readyProcessList, runningProcessList, waitingProcessList, termProcessList;
@@ -26,60 +29,52 @@ public class Clock implements Runnable{
         theScheduler = new Scheduler();
         clockSpeed = 1000;
         paused = true;
+        timeQuantum = 0;
         thread = new Thread(this);
         thread.start();
     }
-    
     public void run(){
         while (true){  
             if(paused == false){
-                theScheduler.loadProcesses(allProcesses, newProcessList, readyProcessList, runningProcessList, waitingProcessList, termProcessList);
                 incrementTime();
                 label.setText(Integer.toString(currentTime));
-                theScheduler.checkProcessStatus(currentTime);
                 try{ Thread.sleep(clockSpeed); } catch (Exception e) {}
             }
         }
     }
-    
     public void setClockSpeed(int speed){
         clockSpeed = speed;
     }
-    
     public void incrementTime(){
+        theScheduler.loadProcesses(allProcesses, newProcessList, readyProcessList, runningProcessList, waitingProcessList, termProcessList);
         currentTime++;
+        theScheduler.checkProcessStatusTemp(currentTime, timeQuantum);
     }
-    
     public int getCurrentTime(){
         return currentTime;
     }
-    
     public void setCurrentTime(int ct){
         currentTime = ct;
     }
-    
     public void startClock(){
         paused = false;
     }
-    
     public void stopClock(){
         paused = true;
     }
-    
     public Boolean isPaused(){
         return paused;
     }
-    
     public void prepareScheduler(ArrayList<Process> a, ArrayList<Process> n, ArrayList<Process> r, 
-            ArrayList<Process> run, ArrayList<Process> w, ArrayList<Process> t){
+            ArrayList<Process> run, ArrayList<Process> w, ArrayList<Process> t, int tq){
         allProcesses = a;
         newProcessList = n;
         readyProcessList = r;
         runningProcessList = run;
         waitingProcessList = w;
         termProcessList = t;
+        timeQuantum = tq;
     }
-    
     public static void main(String args[]) 
     {
         System.out.println("ClockStarter test code");
