640e96ee2087a6433c21f2afeb4d50f6be1eeb16
IDZeta <36170408+IDZeta@users.noreply.github.com>
2018-02-25 18:33:52 -0500

    Fixed Decrement Issues
    -Processes no longer do work when entering Running
    -Processes no longer decrement to -1 before being moved to Waiting

--- Project2Files/DilyardProject2/src/proj2/Simulator.java
+++ Project2Files/DilyardProject2/src/proj2/Simulator.java
@@ -33,28 +33,47 @@ public class Simulator{
         this.waitingProcessList = w;
         this.termProcessList = t;
     }
-    public void checkProcessStatus(int ct){
-        //If a Process is in New, after 1 system cycle, move it to Ready
-        if(newProcessList.isEmpty() == false){
-            for(int i = 0; i < newProcessList.size(); i++) {
-                Process toCheck = newProcessList.get(i);
-                int creationTime = toCheck.getCreationTime();
-                if(creationTime+1 == ct){
-                    readyProcessList.add(toCheck);   
+    public void rebuildTraceTape(ArrayList<String> splitTraceTape, Process p){
+        //Since we need to remake Trace Tapes so often, might as well have a method for it
+        String TTUpdate = splitTraceTape.get(0)+" ";
+        for(int i = 1; i < splitTraceTape.size(); i++) {
+            TTUpdate += splitTraceTape.get(i);
+            if(i != splitTraceTape.size()){
+                TTUpdate += " ";
+            }
+        }
+        p.setTraceTape(TTUpdate);
+    }
+    public void checkProcessStatus(int ct, int tq){
+        //If at least one Process is in Waiting, see if it's done Waiting
+        if(waitingProcessList.isEmpty() == false){
+            String waitingTraceTape = waitingProcessList.get(0).getTraceTape();
+            String[] tempTraceTapeCharacters = waitingTraceTape.split(" ");
+            //Putting the Trace Tape into an ArrayList makes it easier to remove things
+            ArrayList<String> splitTraceTape = new ArrayList<>();
+            for(int i = 0; i < tempTraceTapeCharacters.length; i++) {
+                splitTraceTape.add(tempTraceTapeCharacters[i]);
+            }
+            //Check how long each Process has been Waiting
+            for(int i = 0; i < waitingProcessList.size(); i++) {
+                int waitCycles = Integer.parseInt(splitTraceTape.get(1));
+                System.out.println("A Process in Waiting has "+waitCycles+" cycles left");
+                if(waitCycles > 0){
+                    //If an "I _" block isn't finished, decrement the time remaining
+                    waitCycles--;
+                    splitTraceTape.set(1, Integer.toString(waitCycles));
+                    rebuildTraceTape(splitTraceTape, waitingProcessList.get(i));
+                }else{ //If it IS finished, remove that block and move the Process into Ready
+                    System.out.println("\tA Process is done Waiting");
+                    splitTraceTape.remove(1);
+                    splitTraceTape.remove(0);
+                    rebuildTraceTape(splitTraceTape, waitingProcessList.get(i));
+                    listToSort.add(waitingProcessList.get(i));
+                    waitingProcessList.remove(i);
+                    i--; //Ensures that a member of the list isn't skipped when something is removed
                 }
             }
-            newProcessList.clear();
         }
-        //If a Process's creation time matches the current time, move it to New
-        for(int i = 0; i < allProcesses.size(); i++) {
-            Process toCheck = allProcesses.get(i);
-            int creationTime = toCheck.getCreationTime();
-            if(creationTime == ct){
-                newProcessList.add(toCheck);
-            }
-        }
-    }
-    public void checkProcessStatusTemp(int ct, int tq){
         //If nothing is Running, add something to Running
         if(readyProcessList.isEmpty() == false && runningProcessList.isEmpty() == true){
             runningProcessList.add(readyProcessList.get(0));
@@ -72,11 +91,11 @@ public class Simulator{
                 splitTraceTape.add(tempTraceTapeCharacters[i]);
             }
             //If the Time Quantum hasn't expired, check the Trace Tape
-            if(runningCounter != tq){
+            if(runningCounter != tq+1){
                 int runCycles = Integer.parseInt(splitTraceTape.get(1));
                 System.out.println("\t\tnumCycles "+runCycles);
                 //If a "C _" block is finished, remove it
-                if(runCycles < 0){
+                if(runCycles <= 0){
                     System.out.println("\tThe Running Process is done using the CPU");
                     splitTraceTape.remove(1);
                     splitTraceTape.remove(0);
@@ -91,14 +110,7 @@ public class Simulator{
                         //If the Trace Tape is not empty, go to Waiting
                         //Update the Trace Tape first
                         Process tempP = runningProcessList.get(0);
-                        String TTUpdate = splitTraceTape.get(0)+" ";
-                        for(int i = 1; i < splitTraceTape.size(); i++){
-                            TTUpdate += splitTraceTape.get(i);
-                            if(i != splitTraceTape.size()){
-                                TTUpdate += " ";
-                            }
-                        }
-                        tempP.setTraceTape(TTUpdate);
+                        rebuildTraceTape(splitTraceTape, tempP);
                         waitingProcessList.add(tempP);
                         runningProcessList.remove(0);
                         runningCounter = -1;
@@ -106,76 +118,21 @@ public class Simulator{
                     }
                 }else{ //If not finished with the CPU, decrement the number of cycles left
                     System.out.println("\tThe Running Process needs more time");
-                    runCycles--;
+                    if(runningCounter != 0){
+                        runCycles--;
+                    }
                     //Update the Trace Tape first
                     splitTraceTape.set(1, Integer.toString(runCycles));
-                    String TTUpdate = splitTraceTape.get(0)+" ";
-                    for(int i = 1; i < splitTraceTape.size(); i++){
-                        TTUpdate += splitTraceTape.get(i);
-                        if(i != splitTraceTape.size()){
-                            TTUpdate += " ";
-                        }
-                    }
-                    runningProcessList.get(0).setTraceTape(TTUpdate);
+                    rebuildTraceTape(splitTraceTape, runningProcessList.get(0));
                 }
             }else{ //If the Time Quantum has expired, move it back to Ready
                 System.out.println("\tThe Time Quantum has expired");
                 //Update the Trace Tape first
                 Process tempP = runningProcessList.get(0);
-                String TTUpdate = splitTraceTape.get(0)+" ";
-                for(int i = 1; i < splitTraceTape.size(); i++){
-                    TTUpdate += splitTraceTape.get(i);
-                    if(i != splitTraceTape.size()){
-                        TTUpdate += " ";
-                    }
-                }
-                tempP.setTraceTape(TTUpdate);
+                rebuildTraceTape(splitTraceTape, tempP);
                 listToSort.add(tempP);
                 runningProcessList.remove(0);
                 runningCounter = -1;
-            }
-        }
-        //Logic for moving out of Waiting goes here...
-        if(waitingProcessList.isEmpty() == false){
-            String waitingTraceTape = waitingProcessList.get(0).getTraceTape();
-            String[] tempTraceTapeCharacters = waitingTraceTape.split(" ");
-            //Putting the Trace Tape into an ArrayList makes it easier to remove things
-            ArrayList<String> splitTraceTape = new ArrayList<>();
-            for(int i = 0; i < tempTraceTapeCharacters.length; i++) {
-                splitTraceTape.add(tempTraceTapeCharacters[i]);
-            }
-            //Check how long each Process has been Waiting
-            for(int i = 0; i < waitingProcessList.size(); i++) {
-                int waitCycles = Integer.parseInt(splitTraceTape.get(1));
-                System.out.println("A Process in Waiting has "+waitCycles+" cycles left");
-                if(waitCycles > 0){
-                    //If an "I _" block isn't finished, decrement the time remaining
-                    waitCycles--;
-                    splitTraceTape.set(1, Integer.toString(waitCycles));
-                    String TTUpdate = splitTraceTape.get(0)+" ";
-                    for(int j = 1; j < splitTraceTape.size(); j++) {
-                        TTUpdate += splitTraceTape.get(j);
-                        if(j != splitTraceTape.size()){
-                            TTUpdate += " ";
-                        }
-                    }
-                    waitingProcessList.get(i).setTraceTape(TTUpdate);
-                }else{ //If it IS finished, remove that block and move the Process into Ready
-                    System.out.println("\tA Process is done Waiting");
-                    splitTraceTape.remove(1);
-                    splitTraceTape.remove(0);
-                    String TTUpdate = splitTraceTape.get(0)+" ";
-                    for(int j = 1; j < splitTraceTape.size(); j++) {
-                        TTUpdate += splitTraceTape.get(j);
-                        if(j != splitTraceTape.size()){
-                            TTUpdate += " ";
-                        }
-                    }
-                    waitingProcessList.get(i).setTraceTape(TTUpdate);
-                    listToSort.add(waitingProcessList.get(i));
-                    waitingProcessList.remove(i);
-                    i--; //Ensures that a member of the list isn't skipped when something is removed
-                }
             }
         }  
         //If a Process is in New, after 1 system cycle, move it to Ready
