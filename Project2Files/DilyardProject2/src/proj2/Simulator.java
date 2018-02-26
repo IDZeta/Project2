5704408f05ad139f61ac995e494deb326b975c07
IDZeta <36170408+IDZeta@users.noreply.github.com>
2018-02-25 19:26:57 -0500

    "Rules" Accommodated For
    -Simulator now adds Processes into Ready based on criteria selected on Interface
    -Still need to add logic for sorting multiple things from New/Waiting at the same time

--- Project2Files/DilyardProject2/src/proj2/Simulator.java
+++ Project2Files/DilyardProject2/src/proj2/Simulator.java
@@ -44,36 +44,7 @@ public class Simulator{
         }
         p.setTraceTape(TTUpdate);
     }
-    public void checkProcessStatus(int ct, int tq){
-        //If at least one Process is in Waiting, see if it's done Waiting
-        if(waitingProcessList.isEmpty() == false){
-            String waitingTraceTape = waitingProcessList.get(0).getTraceTape();
-            String[] tempTraceTapeCharacters = waitingTraceTape.split(" ");
-            //Putting the Trace Tape into an ArrayList makes it easier to remove things
-            ArrayList<String> splitTraceTape = new ArrayList<>();
-            for(int i = 0; i < tempTraceTapeCharacters.length; i++) {
-                splitTraceTape.add(tempTraceTapeCharacters[i]);
-            }
-            //Check how long each Process has been Waiting
-            for(int i = 0; i < waitingProcessList.size(); i++) {
-                int waitCycles = Integer.parseInt(splitTraceTape.get(1));
-                System.out.println("A Process in Waiting has "+waitCycles+" cycles left");
-                if(waitCycles > 0){
-                    //If an "I _" block isn't finished, decrement the time remaining
-                    waitCycles--;
-                    splitTraceTape.set(1, Integer.toString(waitCycles));
-                    rebuildTraceTape(splitTraceTape, waitingProcessList.get(i));
-                }else{ //If it IS finished, remove that block and move the Process into Ready
-                    System.out.println("\tA Process is done Waiting");
-                    splitTraceTape.remove(1);
-                    splitTraceTape.remove(0);
-                    rebuildTraceTape(splitTraceTape, waitingProcessList.get(i));
-                    listToSort.add(waitingProcessList.get(i));
-                    waitingProcessList.remove(i);
-                    i--; //Ensures that a member of the list isn't skipped when something is removed
-                }
-            }
-        }
+    public void checkRunningProcess(int tq){
         //If nothing is Running, add something to Running
         if(readyProcessList.isEmpty() == false && runningProcessList.isEmpty() == true){
             runningProcessList.add(readyProcessList.get(0));
@@ -134,7 +105,43 @@ public class Simulator{
                 runningProcessList.remove(0);
                 runningCounter = -1;
             }
-        }  
+        }
+    }
+    public void checkWaitingProcesses(){
+         //If at least one Process is in Waiting, see if it's done Waiting
+        if(waitingProcessList.isEmpty() == false){
+            String waitingTraceTape = waitingProcessList.get(0).getTraceTape();
+            String[] tempTraceTapeCharacters = waitingTraceTape.split(" ");
+            //Putting the Trace Tape into an ArrayList makes it easier to remove things
+            ArrayList<String> splitTraceTape = new ArrayList<>();
+            for(int i = 0; i < tempTraceTapeCharacters.length; i++) {
+                splitTraceTape.add(tempTraceTapeCharacters[i]);
+            }
+            //Check how long each Process has been Waiting
+            for(int i = 0; i < waitingProcessList.size(); i++) {
+                int waitCycles = Integer.parseInt(splitTraceTape.get(1));
+                System.out.println("A Process in Waiting has "+waitCycles+" cycles left");
+                if(waitCycles > 0){
+                    //If an "I _" block isn't finished, decrement the time remaining
+                    waitCycles--;
+                    splitTraceTape.set(1, Integer.toString(waitCycles));
+                    rebuildTraceTape(splitTraceTape, waitingProcessList.get(i));
+                }else{ //If it IS finished, remove that block and move the Process into Ready
+                    System.out.println("\tA Process is done Waiting");
+                    splitTraceTape.remove(1);
+                    splitTraceTape.remove(0);
+                    rebuildTraceTape(splitTraceTape, waitingProcessList.get(i));
+                    listToSort.add(waitingProcessList.get(i));
+                    waitingProcessList.remove(i);
+                    i--; //Ensures that a member of the list isn't skipped when something is removed
+                }
+            }
+        }
+        if(listToSort.size() > 1){
+            Collections.sort(listToSort);
+        }
+    }
+    public void checkNewProcesses(int ct){
         //If a Process is in New, after 1 system cycle, move it to Ready
         if(newProcessList.isEmpty() == false){ 
             for(int i = 0; i < newProcessList.size(); i++) {
@@ -154,14 +161,32 @@ public class Simulator{
                 newProcessList.add(toCheck);
             }
         }
-        //Sort everything moving into Ready before adding it to the queue
-        if(listToSort.isEmpty() == false){
-            Collections.sort(listToSort); //Sort Processes by Creation Time
-            readyProcessList.addAll(listToSort);
-            listToSort.clear();
+        if(listToSort.size() > 1){
+            Collections.sort(listToSort);
         }
-        else if(listToSort.size() == 1){
-            readyProcessList.add(listToSort.get(0));
+    }
+    public void checkProcesses(int ct, int tq, String order){
+        if(order.equals("NRW")){
+            checkNewProcesses(ct);
+            checkRunningProcess(tq);
+            checkWaitingProcesses();
         }
+        else if(order.equals("NWR")){
+            checkNewProcesses(ct);
+            checkWaitingProcesses();
+            checkRunningProcess(tq);
+        }
+        else if(order.equals("WNR")){
+            checkWaitingProcesses();
+            checkNewProcesses(ct);
+            checkRunningProcess(tq);
+        }
+        else if(order.equals("WRN")){
+            checkWaitingProcesses();
+            checkRunningProcess(tq);
+            checkNewProcesses(ct);
+        }
+        readyProcessList.addAll(listToSort);
+        listToSort.clear();
     }
 }
